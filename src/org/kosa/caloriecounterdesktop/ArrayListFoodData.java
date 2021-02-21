0618f546b7226d6428fc8f0c21feaf1a77e7ccdb
kosa44 <lukasz.gluch1989@wp.pl>
2021-02-21 20:36:59 +0100

    add methods serializeProductsToJson and deserializeProductsFromJson

--- src/org/kosa/caloriecounterdesktop/ArrayListFoodData.java
+++ src/org/kosa/caloriecounterdesktop/ArrayListFoodData.java
@@ -1,37 +1,70 @@
 package org.kosa.caloriecounterdesktop;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.reflect.Array;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
 
 public class ArrayListFoodData {
     private static ArrayList<Foodstuff> foodData;
 
-    public void createDatabase() {
-        foodData = new ArrayList<Foodstuff>();
-
-        Foodstuff apple = new Foodstuff("Jabłko", 0.40, 0.40, 12.10, 46, 100);
-        Foodstuff cheeseGouda = new Foodstuff("Ser Gouda tłusty", 27.9, 22.9, 0.1, 316, 100);
-        Foodstuff cheeseMozarella = new Foodstuff("Ser Mozarella tłusty", 20.0, 16.0, 0.0, 224, 100);
-        Foodstuff chickenBreast = new Foodstuff("Kurczak, pierś bez skóry", 21.5, 13.0, 0.0, 100, 100);
-        Foodstuff cottageCheese = new Foodstuff("Serek wiejski", 12.3, 4.3, 3.3, 101, 100);
-        Foodstuff fromageFrais = new Foodstuff("Ser twarogowy chudy", 19.8, 0.5, 3.5, 99, 100);
-        Foodstuff lentils = new Foodstuff("Soczewica czerwona", 25.4, 3.0, 57.5, 327, 100);
-        Foodstuff milk = new Foodstuff("Mleko", 3.3, 3.2, 4.8, 61, 100);
-        Foodstuff oatmeal = new Foodstuff("Płatki owsiane", 11.9, 7.2, 69.3, 366, 100);
-        Foodstuff porkChop = new Foodstuff("Wieprzowina, schab", 21.0, 10.0, 0.0, 174, 100);
-        Foodstuff porkNeck = new Foodstuff("Wieprzowina, karkówka", 16.1, 22.8, 0.0, 270, 100);
-        Foodstuff potatoes = new Foodstuff("Ziemniaki", 1.7, 0.1, 19.9, 87, 100);
-        Foodstuff riceWhite = new Foodstuff("Ryż biały", 6.7, 7.0, 78.9, 344, 100);
-        Foodstuff tunaInOil = new Foodstuff("Tuńczyk w oleju", 27.1, 9.0, 0.0, 190, 100);
-        Foodstuff wholeEgg = new Foodstuff("Jajka", 12.5, 10.7, 1.0, 150, 100);
-        Foodstuff[] array = { apple, cheeseGouda, cheeseMozarella, chickenBreast, cottageCheese, fromageFrais, lentils, milk,
-                oatmeal, porkChop, porkNeck, potatoes, riceWhite, tunaInOil, wholeEgg };
-        sortDatabaseByName(array);
-        for (int i = 0; i < array.length; i++) {
-            foodData.add(array[i]);
+    public void serializeProductsToJson(){
+        try (Writer writer = new FileWriter("C:/Users/lenovo/IdeaProjects/CalorieCounterDesktop/resources/initialProducts.json")) {
+            Type foodstuffArrayList = new TypeToken<ArrayList<Foodstuff>>(){}.getType();
+            Gson gson = new GsonBuilder().create();
+            gson.toJson(foodData, foodstuffArrayList, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public ArrayList<Foodstuff> getData() {
+    public ArrayList<Foodstuff> deserializeProductsFromJson() {
+        try {
+            Gson gson = new GsonBuilder().create();
+            Type foodstuffArrayList = new TypeToken<ArrayList<Foodstuff>>(){}.getType();
+            return gson.fromJson("resources/initialProducts.json", foodstuffArrayList);
+        } catch (JsonParseException e) {
+            System.out.println("Couldn't deserialize from json file");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void createDatabase() {
+    foodData = deserializeProductsFromJson();
+//        foodData = new ArrayList<Foodstuff>();
+//
+//        Foodstuff apple = new Foodstuff("Jabłko", 0.40, 0.40, 12.10, 46, 100);
+//        Foodstuff cheeseGouda = new Foodstuff("Ser Gouda tłusty", 27.9, 22.9, 0.1, 316, 100);
+//        Foodstuff cheeseMozarella = new Foodstuff("Ser Mozarella tłusty", 20.0, 16.0, 0.0, 224, 100);
+//        Foodstuff chickenBreast = new Foodstuff("Kurczak, pierś bez skóry", 21.5, 13.0, 0.0, 100, 100);
+//        Foodstuff cottageCheese = new Foodstuff("Serek wiejski", 12.3, 4.3, 3.3, 101, 100);
+//        Foodstuff fromageFrais = new Foodstuff("Ser twarogowy chudy", 19.8, 0.5, 3.5, 99, 100);
+//        Foodstuff lentils = new Foodstuff("Soczewica czerwona", 25.4, 3.0, 57.5, 327, 100);
+//        Foodstuff milk = new Foodstuff("Mleko", 3.3, 3.2, 4.8, 61, 100);
+//        Foodstuff oatmeal = new Foodstuff("Płatki owsiane", 11.9, 7.2, 69.3, 366, 100);
+//        Foodstuff porkChop = new Foodstuff("Wieprzowina, schab", 21.0, 10.0, 0.0, 174, 100);
+//        Foodstuff porkNeck = new Foodstuff("Wieprzowina, karkówka", 16.1, 22.8, 0.0, 270, 100);
+//        Foodstuff potatoes = new Foodstuff("Ziemniaki", 1.7, 0.1, 19.9, 87, 100);
+//        Foodstuff riceWhite = new Foodstuff("Ryż biały", 6.7, 7.0, 78.9, 344, 100);
+//        Foodstuff tunaInOil = new Foodstuff("Tuńczyk w oleju", 27.1, 9.0, 0.0, 190, 100);
+//        Foodstuff wholeEgg = new Foodstuff("Jajka", 12.5, 10.7, 1.0, 150, 100);
+//        Foodstuff[] array = { apple, cheeseGouda, cheeseMozarella, chickenBreast, cottageCheese, fromageFrais, lentils, milk,
+//                oatmeal, porkChop, porkNeck, potatoes, riceWhite, tunaInOil, wholeEgg };
+//        sortDatabaseByName(array);
+//            foodData.addAll(Arrays.asList(array));
+//        serializeProductsToJson();
+    }
+
+    public List<Foodstuff> getData() {
         return foodData;
     }
 
