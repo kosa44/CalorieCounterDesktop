f65f8d36908c0dff3423b544437fc752b19f9398
kosa44 <lukasz.gluch1989@wp.pl>
2021-03-02 20:38:45 +0100

    improve transaction handling, use connection as a local variable per method

--- src/org/kosa/caloriecounterdesktop/model/FoodDatabaseSQLite.java
+++ src/org/kosa/caloriecounterdesktop/model/FoodDatabaseSQLite.java
@@ -8,35 +8,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class FoodDatabaseSQLite implements FoodDatabaseInterface {
-    private static Connection conn = null;
-
-    public FoodDatabaseSQLite() {
-        String url = "jdbc:sqlite:CalorieCounterProducts.db";
-        try {
-            conn = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
+    private String dbUrl = "jdbc:sqlite:CalorieCounterProducts.db";
 
     public ResultSet getName(String name) throws SQLException {
-        Statement state = conn.createStatement();
-        ResultSet res = state.executeQuery("SELECT name FROM CalorieCounterProducts WHERE name = " + "'" + name + "'");
-        return res;
+        Connection conn = DriverManager.getConnection(dbUrl);
+        Statement stmt = conn.createStatement();
+        return stmt.executeQuery("SELECT name FROM CalorieCounterProducts WHERE name = " + "'" + name + "'");
     }
 
     public ResultSet getProtein(String name) throws SQLException {
-        Statement state = conn.createStatement();
-        ResultSet res = state
-                .executeQuery("SELECT protein FROM CalorieCounterProducts WHERE name = " + "'" + name + "'");
-        return res;
+        Connection conn = DriverManager.getConnection(dbUrl);
+        Statement stmt = conn.createStatement();
+        return stmt.executeQuery("SELECT protein FROM CalorieCounterProducts WHERE name = " + "'" + name + "'");
     }
 
     public ResultSet displayProducts() throws SQLException {
-        Statement state = conn.createStatement();
-        ResultSet res = state
-                .executeQuery("SELECT name, protein, fats, carbohydrates, calories, grams FROM CalorieCounterProducts");
-        return res;
+        Connection conn = DriverManager.getConnection(dbUrl);
+        Statement stmt = conn.createStatement();
+        return stmt.executeQuery("SELECT name, protein, fats, carbohydrates, calories, grams FROM CalorieCounterProducts");
     }
 
     @Override
@@ -47,36 +36,47 @@ public class FoodDatabaseSQLite implements FoodDatabaseInterface {
 
     @Override
     public void addToDatabase(Foodstuff food) throws SQLException {
-        try (PreparedStatement pstmt = conn.prepareStatement(
-                "INSERT INTO CalorieCounterProducts(name, protein, fats, carbohydrates, calories, grams) VALUES (?,?,?,?,?,?);")) {
-            conn.setAutoCommit(false);
-            pstmt.setString(1, food.getName());
-            pstmt.setDouble(2, food.getProtein());
-            pstmt.setDouble(3, food.getFat());
-            pstmt.setDouble(4, food.getCarbohydrates());
-            pstmt.setDouble(5, food.getCalories());
-            pstmt.setDouble(6, food.getGrams());
-            pstmt.execute();
-            conn.commit();
+        try (Connection conn = DriverManager.getConnection("jdbc:sqlite:CalorieCounterProducts.db")) {
+            System.out.println("Conn is " + conn.toString());
+            boolean formerAutoCommitMode = conn.getAutoCommit();
+            try (PreparedStatement pstmt = conn.prepareStatement("INSERT INTO CalorieCounterProducts(name, protein, fats, carbohydrates, calories, grams) VALUES (?,?,?,?,?,?);")) {
+                conn.setAutoCommit(false);
+                pstmt.setString(1, food.getName());
+                pstmt.setDouble(2, food.getProtein());
+                pstmt.setDouble(3, food.getFat());
+                pstmt.setDouble(4, food.getCarbohydrates());
+                pstmt.setDouble(5, food.getCalories());
+                pstmt.setDouble(6, food.getGrams());
+                pstmt.execute();
+                System.out.println(pstmt.toString());
+                conn.commit();
+            } catch (SQLException e) {
+                System.out.println("Failed to add new product to the database, transaction is being rolled back");
+                conn.rollback();
+                e.printStackTrace();
+            }
+            conn.setAutoCommit(formerAutoCommitMode);
         } catch (SQLException e) {
-            conn.rollback();
-            System.out.println("Transaction is being rolled back");
             e.printStackTrace();
         }
     }
 
     @Override
-    public void removeFromDatabase(String name) throws SQLException {
-        String sql = "DELETE FROM CalorieCounterProducts WHERE name = ?";
-
-        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            conn.setAutoCommit(false);
-            pstmt.setString(1, name);
-            pstmt.execute();
-            conn.commit();
+    public void removeFromDatabase(String name) {
+        try (Connection conn = DriverManager.getConnection(dbUrl)) {
+            boolean formerAutoCommitMode = conn.getAutoCommit();
+            try (PreparedStatement pstmt = conn.prepareStatement("DELETE FROM CalorieCounterProducts WHERE name = ?")) {
+                conn.setAutoCommit(false);
+                pstmt.setString(1, name);
+                pstmt.execute();
+                conn.commit();
+            } catch (SQLException e) {
+                System.out.println("Failed to remove product " + name + " from the database, transaction is being rolled back");
+                conn.rollback();
+                e.printStackTrace();
+            }
+            conn.setAutoCommit(formerAutoCommitMode);
         } catch (SQLException e) {
-            conn.rollback();
-            System.out.println("Transaction is being rolled back");
             e.printStackTrace();
         }
     }
@@ -85,59 +85,61 @@ public class FoodDatabaseSQLite implements FoodDatabaseInterface {
         if (hasData()) {
             return;
         }
-        // check for database table
-        ArrayListFoodData foodArray = new ArrayListFoodData();
-        foodArray.createDatabase();
-        System.out.println(foodArray.getData().get(0));
-        boolean formerAutoCommitMode = conn.getAutoCommit();
-        Statement state = conn.createStatement();
-        ResultSet res = state.executeQuery(
-                "SELECT name FROM sqlite_master WHERE type='table' AND name='CalorieCounterProducts'");
-        if (!res.next()) {
-            System.out.println("Building the CalorieCounterProducts table with pre-populated values.");
-            // need to build the table
+        //check for database table
+        try (Connection conn = DriverManager.getConnection(dbUrl)) {
+            boolean formerAutoCommitMode = conn.getAutoCommit();
             conn.setAutoCommit(false);
-            try (Statement state2 = conn.createStatement()) {
-                state2.executeUpdate("CREATE TABLE CalorieCounterProducts(id integer," + "name varchar(60),"
-                        + "protein real," + "fats real," + "carbohydrates real," + "calories real," + "grams real,"
-                        + "primary key (id));");
-                conn.commit();
+            String sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='CalorieCounterProducts'";
+            try (Statement stmt = conn.createStatement(); ResultSet res = stmt.executeQuery(sql)) {
+                if (!res.next()) {
+                    //need to build the table
+                    System.out.println("Building the CalorieCounterProducts table with pre-populated values.");
+                    Statement stmt2 = conn.createStatement();
+                    stmt2.executeUpdate("CREATE TABLE CalorieCounterProducts(id integer," + "name varchar(60),"
+                            + "protein real," + "fats real," + "carbohydrates real," + "calories real," + "grams real,"
+                            + "primary key (id));");
+                    stmt2.close();
+                    conn.commit();
+                }
             } catch (SQLException e) {
+                System.out.println("Failed to create products table, transaction is being rolled back");
                 conn.rollback();
-                System.out.println("Transaction is being rolled back");
                 e.printStackTrace();
             }
             conn.setAutoCommit(formerAutoCommitMode);
         }
         if (hasData() == false) {
-            insertInitialData();
+        insertInitialData();
         }
     }
 
     public void insertInitialData() throws SQLException {
-        // inserting data
-        ArrayListFoodData foodArray = new ArrayListFoodData();
-        foodArray.createDatabase();
-        boolean formerAutoCommitMode = conn.getAutoCommit();
-        for (int i = 0; i < foodArray.getData().size(); i++) {
-            try (PreparedStatement prep = conn.prepareStatement(
-                    "INSERT INTO CalorieCounterProducts(name, protein, fats, carbohydrates, calories, grams) VALUES (?,?,?,?,?,?);")) {
-                conn.setAutoCommit(false);
-                prep.setString(1, foodArray.getFoodstuff(i).getName());
-                prep.setDouble(2, foodArray.getFoodstuff(i).getProtein());
-                prep.setDouble(3, foodArray.getFoodstuff(i).getFat());
-                prep.setDouble(4, foodArray.getFoodstuff(i).getCarbohydrates());
-                prep.setDouble(5, foodArray.getFoodstuff(i).getCalories());
-                prep.setDouble(6, foodArray.getFoodstuff(i).getGrams());
-                prep.execute();
-                conn.commit();
-            } catch (SQLException e) {
-                conn.rollback();
-                System.out.println("Transaction is being rolled back");
-                e.printStackTrace();
-            }
+        try (Connection conn = DriverManager.getConnection(dbUrl)) {
+            ArrayListFoodData foodArray = new ArrayListFoodData();
+            foodArray.createDatabase();
+            boolean formerAutoCommitMode = conn.getAutoCommit();
+            String sql = "INSERT INTO CalorieCounterProducts(name, protein, fats, carbohydrates, calories, grams) VALUES (?,?,?,?,?,?);";
+//            for (int i = 0; i < foodArray.getData().size(); i++) {
+                try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                    conn.setAutoCommit(false);
+                for (int i = 0; i < foodArray.getData().size(); i++) {
+                    pstmt.setString(1, foodArray.getFoodstuff(i).getName());
+                    pstmt.setDouble(2, foodArray.getFoodstuff(i).getProtein());
+                    pstmt.setDouble(3, foodArray.getFoodstuff(i).getFat());
+                    pstmt.setDouble(4, foodArray.getFoodstuff(i).getCarbohydrates());
+                    pstmt.setDouble(5, foodArray.getFoodstuff(i).getCalories());
+                    pstmt.setDouble(6, foodArray.getFoodstuff(i).getGrams());
+                    pstmt.execute();
+                }
+                    conn.commit();
+                } catch (SQLException e) {
+                    System.out.println("Failed to insert initial data, transaction is being rolled back");
+                    conn.rollback();
+                    e.printStackTrace();
+                }
+//            }
+            conn.setAutoCommit(formerAutoCommitMode);
         }
-        conn.setAutoCommit(formerAutoCommitMode);
     }
 
     @Override
@@ -149,7 +151,9 @@ public class FoodDatabaseSQLite implements FoodDatabaseInterface {
     @Override
     public ResultSet executeQuery(String sql) {
         try {
+            Connection conn = DriverManager.getConnection(dbUrl);
             Statement stmt = conn.createStatement();
+
             return stmt.executeQuery(sql);
         } catch (SQLException e) {
             e.printStackTrace();
@@ -158,19 +162,18 @@ public class FoodDatabaseSQLite implements FoodDatabaseInterface {
     }
 
     private boolean hasData() {
-        try {
-            Statement state = conn.createStatement();
-            ResultSet res = state.executeQuery(
-                    "SELECT * FROM CalorieCounterProducts");
-            System.out.println(res.getRow());
+        try (Connection conn = DriverManager.getConnection(dbUrl); Statement stmt = conn.createStatement()) {
+            ResultSet res = stmt.executeQuery(
+                    "SELECT name FROM CalorieCounterProducts");
             if (!res.next()) {
+                res.close();
                 return false;
             }
             if (res.next()) {
+                res.close();
                 return true;
             }
         } catch (SQLException e) {
-            System.out.println("Couldn't connect to the database");
             e.printStackTrace();
         }
         return false;
