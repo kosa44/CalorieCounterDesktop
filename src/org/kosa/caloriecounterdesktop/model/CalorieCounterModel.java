0ec21194c7bb1d77b8a57eb5e3c1a158f501d2e5
kosa44 <lukasz.gluch1989@wp.pl>
2021-03-09 19:30:49 +0100

    fix sqlite busy error, caused by an open connection after constructor call, add hasData method

--- src/org/kosa/caloriecounterdesktop/model/CalorieCounterModel.java
+++ src/org/kosa/caloriecounterdesktop/model/CalorieCounterModel.java
@@ -1,5 +1,7 @@
 package org.kosa.caloriecounterdesktop.model;
 
+import com.google.gson.internal.bind.SqlDateTypeAdapter;
+
 import java.io.File;
 import java.sql.*;
 
@@ -15,24 +17,31 @@ public class CalorieCounterModel implements CalorieCounterModelInterface {
         } catch (SQLException e2) {
             e2.printStackTrace();
         }
-        try {
-            Connection conn = DriverManager.getConnection(sessionNamesURL);
+        if (hasData()) {
+            return;
+        }
+        String sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='SessionNames'";
+        try (Connection conn = DriverManager.getConnection(sessionNamesURL); Statement stmt = conn.createStatement();
+             ResultSet res = stmt
+                     .executeQuery(sql);) {
+            boolean formerAutoCommit = conn.getAutoCommit();
             // check for database table
-            if (!hasData) {
-                hasData = true;
-                Statement state = conn.createStatement();
-                ResultSet res = state
-                        .executeQuery("SELECT name FROM sqlite_master WHERE type='table' AND name='SessionNames'");
-                if (!res.next()) {
-                    // need to build the table
-                    System.out.println("Building the SessionNames table with pre-populated values.");
-                    Statement state2 = conn.createStatement();
-                    state2.executeUpdate(
+            if (!res.next()) {
+                // need to build the table
+                try (Statement stmt2 = conn.createStatement()) {
+                    conn.setAutoCommit(false);
+                    stmt2.executeUpdate(
                             "CREATE TABLE SessionNames(id integer," + "name varchar(60)," + "primary key (id));");
+                    System.out.println("Building the SessionNames table with pre-populated values.");
+                    conn.commit();
+                } catch (SQLException e) {
+                    conn.rollback();
+                    conn.setAutoCommit(formerAutoCommit);
+                    e.printStackTrace();
                 }
             }
-        } catch (SQLException e1) {
-            e1.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 
@@ -99,14 +108,14 @@ public class CalorieCounterModel implements CalorieCounterModelInterface {
         try (Connection conn = DriverManager.getConnection(sessionNamesURL)) {
             boolean formerAutoCommitMode = conn.getAutoCommit();
             conn.setAutoCommit(false);
-            File file = new File("/"+name+".db");
+            File file = new File("/" + name + ".db");
             try (PreparedStatement pstmt = conn.prepareStatement("DELETE FROM SessionNames WHERE name = ?")) {
                 if (file.delete()) {
-                    pstmt.setString(1,name);
+                    pstmt.setString(1, name);
                     pstmt.execute();
                     conn.commit();
                 } else {
-                    System.out.println("Failed to delete file "+name);
+                    System.out.println("Failed to delete file " + name);
                 }
             } catch (SQLException e) {
                 System.out.println("Failed to remove session, transaction is being rolled back");
@@ -117,6 +126,24 @@ public class CalorieCounterModel implements CalorieCounterModelInterface {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private boolean hasData() {
+        try (Connection conn = DriverManager.getConnection(sessionNamesURL); Statement stmt = conn.createStatement()) {
+            ResultSet res = stmt.executeQuery(
+                    "SELECT name FROM SessionNames");
+            if (!res.next()) {
+                res.close();
+                return false;
+            }
+            if (res.next()) {
+                res.close();
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
 
